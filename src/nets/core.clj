187c1a81fddc14864712b86f1599ed32dfa26526
Stefan Vukicevic <stefanvuk17@gmail.com>
2018-01-24 15:04:06 -0800

    Finishes converting old functions to work with expanded net structure.

--- src/nets/core.clj
+++ src/nets/core.clj
@@ -6,27 +6,35 @@
             [clojure.math.numeric-tower])
   (:gen-class))
 
+;;; This namespace implements the backpropogation algorithm. It follows the convention
+;;; that vectors are row vectors (as opposed to column vectors) so matrix
+;;; multiplication is (v * M).
 (defmacro in-net
-  "ANAPHORIC, binds unqualified symbols: matrices biases act-fns deriv-fns."
+  "ANAPHORIC MACRO, binds unqualified symbols: matrices biases act-fns deriv-fns
+  to vectors corresponding to the layer data. For example,
+  (nth matrices 0) === (nth (:layers net) 0)  ."
   [net body]
   `(let [~'matrices (mapv #(:matrix %) (:layers ~net))
          ~'biases (mapv #(:bias %) (:layers ~net))
          ~'act-fns (mapv #(:act-fn %) (:layers ~net))
-         ~'deriv-fns (mapv #(:deriv-fns %) (:layers ~net))]
+         ~'deriv-fns (mapv #(:deriv-fn %) (:layers ~net))]
      ~body))
 
+; TODO: cache pre-activation values as well b/c those are used in backpropogate
 (defn propogate-forward
+  "Propogates an input through the net, returning the intermediate layer outputs. The
+  return vector has the following structure:
+  [input layer-0-output layer-1-output ... output(output-layer-output)]"
   [net input]
-  (if (not (= (:num-inputs net) (count input)))
-    (printf "Error, net was passed input of size %d but its input layer is of size %d.\n"
-            (count input) (:num-inputs net))
-    (in-net net
-            (reductions
-             (fn [out [matrix bias act-fn]]
-               ; Compute the output of the next layer
-               (act-fn (matrix/add (matrix/mmul out matrix) bias)))
-             input
-             (mapv vector matrices biases act-fns)))))
+  {:pre [(= (:num-inputs net) (count input))]
+   :post [(= (count (:layers net)) (dec (count %)))]}
+  (in-net net
+          (reductions
+           (fn [out [matrix bias act-fn]]
+           ;Compute the output of the next layer
+             (act-fn (matrix/add (matrix/mmul out matrix) bias)))
+           input
+           (mapv vector matrices biases act-fns))))
 
 (defn- hprod
   "Returns the componentwise product of vectors V1 and V2."
@@ -41,72 +49,74 @@
   [func vector]
   (mapv #(mapv func %) vector))
 
+(defmacro showlet
+  "Performs a let binding as usual, but prints out the value of each binding immediately
+  after the binding succeeds."
+  [letvec body]
+  (if (empty? letvec)
+    body
+    (let [[symbol expr & r] letvec]
+      `(let [~symbol ~expr]
+         (do (print ~(name symbol) ~symbol)
+             (newline)
+             (showlet ~r ~body))))))
 
 ;; TODO: change fprop so that it caches the right value isntead of recomputing it here
 (defn backpropogate
-  [net fprop-values true-value deriv-error-fn];deriv-error-fn takes vec and tval to err-vec
+  [net fprop-values true-value deriv-error-fn]
+  {:pre [(= (count (last fprop-values)) (count true-value))]
+   :post [(= (count (:layers net)) (count %))]}
   (in-net net
           (let [output (last fprop-values)
                 error-deriv (deriv-error-fn output true-value)
-                rev-inputs (reverse
-                            (mapv
-                            #(%1 (matrix/add (matrix/mmul %2 %3) %4))
-                            deriv-fns
-                            (butlast fprop-values)
-                            matrices
-                            biases))
-                rev-mats (reverse (mapv #(matrix/transpose %) matrices))
-                error-signal (hprod error-deriv (first rev-inputs))]
-            (print 'here)
-            (reverse ; Given that the error propogates backwards, we reverse the result
+                ; Compute the preactivation layer-inputs
+                layer-inputs (mapv #(matrix/add (matrix/mmul %1 %2) %3)
+                                   (butlast fprop-values) matrices biases)
+                ; Compute the error-signal at the output layer
+                error-signal (hprod error-deriv
+                                    ((last deriv-fns) (last layer-inputs)))]
+            (reverse
              (reductions
-              (fn [prev-error-signal [prev-matrix layer-input]]
-                (hprod (matrix/mmul prev-error-signal prev-matrix) layer-input)
-                error-signal
-                (mapv vector rev-mats (rest rev-inputs))))))))
-(defn deltas
-  "Given an error function ERROR-FN (type: VEC -> VCE -> NUM), the pre-activation outputs OUTPUTS of each layer of the net represented
-  by MATRICES, the derivative of the activation function DERIV-ACT-FN (type: VEC -> VEC) used in the forward propogation to obtain OUTPUTS,
-  and the target-value of the computation TARGET-VALUE, deltas will perform backwards propogation of the error, returning
-  a vector of vectors such that the i_th elements of the j_th vector contains the error associated with the i_th neuron of the j_th layer."
-  [matrices net-outputs true-value deriv-act-fn]
-  (let [net-result (last net-outputs)
-        rev-layer-in (reverse
-                      (mapv #(deriv-act-fn (matrix/mmul %1 %2))
-                            (butlast net-outputs) matrices))
-        rev-trans-mats (reverse (mapv #(matrix/transpose %) (rest matrices)))]
-    (reverse
-     (reductions
-      ; The reduction function backpropgates the deltas from the preceding layer
-      (fn [preceding-delta [preceding-matrix-trans layer-input]]
-        (hprod (matrix/mmul preceding-delta preceding-matrix-trans) layer-input))
-      ; The initial value of the reduction is the delta of the outputs layer
-      (hprod (mapv - net-result true-value) (first rev-layer-in))
-      ; Subsequent deltas have to be backpropogated through the matrices
-      (mapv vector rev-trans-mats (rest rev-layer-in))))))
+              (fn [prev-error-signal [prev-matrix layer-deriv]]
+                (hprod (matrix/mmul prev-error-signal prev-matrix)
+                       layer-deriv))
+              error-signal
+              (reverse
+              ; in order to perform backpropogation for the i_th layer
+              ; we need the transpose of the i+1th matrix
+              ; and the derivation of i_th layer input value
+               (mapv vector
+                     (mapv #(matrix/transpose %) (rest matrices))
+                     (mapv #(%1 (matrix/mmul %2 %3))
+                           (butlast deriv-fns)
+                           (butlast layer-inputs)
+                           (butlast matrices)))))))))
+
+(defn deriv-matrices
+  "To calculate the derivative of the error with respect to i_th weight matrix, the equation is
+  y_(i - 1) o delta_i where y_j is the transpose of the output of the j_th layer and d_j the j_th's layer delta. In order to use the matrix multiplication, the arguments (which are vectors of vectors) are converted to vectors of matrices."
+  [fprop-values bprop-values]
+  (let [ys (mapv #(matrix/transpose %) (mapv vector fprop-values))
+        ds (mapv vector bprop-values)]
+    (mapv #(matrix/mmul %1 %2) ys ds)))
 
 (defn weight-update
-  [matrices deltas layer-outputs learning-rate]
-  ;;; TODO: find a nicer way of getting matrices from the vectors...
-  (let [deltas-mats (mapv vector deltas)
-        outputs-transp-mats (mapv #(matrix/transpose (vector %)) (butlast layer-outputs))
-        partials (mapv #(matrix/mmul %1 %2) outputs-transp-mats deltas-mats)]
-        ;;; this map is awkard ^^^ why doesn't matrix.core allow matrix/mmul to work with just vectors?
-    (mapv #(matrix/sub %1 (hprodm (matrix/scale %1 learning-rate) %2))
-          matrices partials)))
-
-
+  "Produces a new net given the layer-outputs FPROP-VALS, layer deltas BPROP_VALS and a learning rate."
+  [net fprop-vals bprop-vals learning-rate]
+  (in-net net
+          (showlet [derivs (deriv-matrices fprop-vals bprop-vals)
+                scaled-matrices (mapv #(matrix/scale learning-rate %) matrices)
+                weight-update-matrices (mapv #(hprodm %1 %2) scaled-matrices derivs)
+                new-matrices (mapv #(matrix/sub %1 %2) matrices weight-update-matrices)]
+            (assoc net :layers
+                   (mapv (fn [layer matrix] (assoc layer :matrix matrix))
+                         (:layers net)
+                         new-matrices)))))
 (defn train
-  "Given INPUT and TARGET-OUTPUT, train updates MATRICES."
-  [matrices input target-output learning-rate act-fun deriv-act-fn]
-  (let [layer-outs (propogate-forward matrices input act-fun)
-        delts (deltas matrices layer-outs target-output deriv-act-fn)]
-    (weight-update matrices delts layer-outs learning-rate)))
-;; notes: erro-fn and deriv-act-fn need to either apply to vectors or to individual numbers... decide.
-
-
-(def mats [[[3 2 0] [-1 0 0]] [[5] [-2] [-8]]])
-(defn error-fn [v1 v2] (mapv - v1 v2))
-(defn  act-fn [v] (mapv #(+ 1 %) v))
-(defn deriv-act-fn [v] (mapv #( * 0.5 %) v))
-(def outputs (propogate-forward mats [1 1] act-fn))
+  "Given an INPUT, a TARGET output, a LEARNING-RATE, and the derivative
+  of the error function, ERROR-DERIV, train will retrun the net
+  resulting from performing backpropogation on this particular input."
+  [net input target learning-rate error-deriv]
+  (let [fvals (propogate-forward net input)
+        bvals (backpropogate net fvals target error-deriv)]
+    (weight-update net fvals bvals learning-rate)))
