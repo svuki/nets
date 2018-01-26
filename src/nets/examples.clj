7aa4f36b4cbcc1e4eb2ba857417439d63e592d16
Stefan Vukicevic <stefanvuk17@gmail.com>
2018-01-25 17:17:26 -0800

    Adds reverse function exmaple.

--- src/nets/examples.clj
+++ src/nets/examples.clj
@@ -14,9 +14,21 @@
   [1])
 
 (defn always-one
-  "In this exmaple, the neural net is trained to always output [1]."
+  "In this exmaple, the net is trained to always output [1]."
   [learning-rate times-to-run]
   (let [test-net (net/new-net 2 [2 :sigmoid] [1 :sigmoid])]
     (core/multi-set-train-verbose
      test-net sample-input-fn sample-output-fn
+     error-fns/l2 error-fns/l2-deriv learning-rate times-to-run)))
+
+(defn reverse-fn
+  [[x y]] [y x])
+
+(defn always-reverse
+  "In this example the net is trained to learn the function
+  [x y] --> [y x]."
+  [learning-rate times-to-run]
+  (let [test-net (net/new-net 2 [2 :tanh] [2 :tanh])]
+    (core/multi-set-train-verbose
+     test-net sample-input-fn reverse-fn
      error-fns/l2 error-fns/l2-deriv learning-rate times-to-run)))
