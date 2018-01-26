d1145c2b3384b08c07227be32f16e6bedb7919e4
Stefan Vukicevic <stefanvuk17@gmail.com>
2018-01-26 02:14:50 -0800

    Add train-for and sample-output functions.
    Introduce the use of passing maps "training-profiles" to runner functions
    to avoid 10 argument functions.

--- src/nets/core.clj
+++ src/nets/core.clj
@@ -29,7 +29,7 @@
           target (target-func input)
           next-net
           (verbose-trainer net input target error-fn deriv-error-fn learning-rate)]
-      (multi-set-train-verbose
+      (recur
        next-net
        input-func
        target-func
@@ -38,20 +38,37 @@
        learning-rate
        (dec count)))))
 
-(defn until-error-multi-set-train-verbose
-  [net input-func target-func error-fn deriv-error-fn learning-rate count max-err]
-  (let [input (input-func)
-        target (target-func input)
-        fvals (backprop/propogate-forward net input)
-        bvals (backprop/backpropogate net fvals target deriv-error-fn)
-        output (last fvals)
-        error (error-fn target output)]
-    (if (< error max-err)
-      (do (printf "Completed in %d runs.\n")
-          net)
-      (let [new-net
-            (verbose-trainer
-             net input target error-fn
-             deriv-error-fn learning-rate)]
-        (recur new-net input-func target-func error-fn deriv-error-fn
-               learning-rate (inc count) max-err)))))
+; TODO: generalize perror, easiest way is to have a metric selection which
+; abstract away err-fn, err-deriv, and the vector size in metric
+(defn perror
+  "Calculates the percent error given an ERROR and a TARGET value.
+  Currently this only works for the l2-metric."
+  [error target]
+  (let [zero-vec (take (count target) (repeat 0))]
+    (/ error (error-fns/l2 target zero-vec))))
+
+(defn sample-output
+  "Produces the output and the error for N iterations of NET under profile
+  TRAINING-PROFILE"
+  [net training-profile sample-count]
+  (let [inputs (take sample-count (repeatedly (:input-fn training-profile)))
+        outputs (mapv #(backprop/net-eval net %) inputs)
+        targets (mapv (:output-fn training-profile) inputs)
+        errors (mapv (:err-fn training-profile) targets outputs)
+        perrors (mapv perror errors targets) ]
+    (clojure.pprint/print-table
+     (mapv #(zipmap [:input :output :target :error :perror]
+                    (vector %1 %2 %3 %4 %5))
+           inputs outputs targets errors perrors))))
+
+
+(defn train-for
+  [net training-profile iterations]
+  (let [input ((:input-fn training-profile))
+        output ((:output-fn training-profile) input)
+        next-net (backprop/train net input output
+                                 (:lrate training-profile)
+                                 (:err-deriv training-profile))]
+    (if (>= 1 iterations)
+      (sample-output next-net training-profile 30)
+      (recur next-net training-profile (dec iterations)))))
