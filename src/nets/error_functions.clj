7fdb961fc9f65de270634dd8c575168d9f36c20f
Stefan Vukicevic <stefanvuk17@gmail.com>
2018-01-28 20:15:11 -0800

    Add cross entropy error function,

--- src/nets/error_functions.clj
+++ src/nets/error_functions.clj
@@ -3,8 +3,10 @@
   (:gen-class))
 
 ;;; This namespace provides various cost functions and their gradients
-;;; By convention, the first argument will be the actual value and the
-;;; second argument the target value
+;;; By convention, the first argument to the cost functions and their gradients
+;;; will be the OBSERVED value and the second argument will be the EXPECTED value.
+;;; All cost functions should have type VEC -> VEC -> REAL while their gradients
+;;; should have type VEC -> VEC -> VEC.
 
 (defn- mean-squared
   "Returns the mean squared error of IDEAL and ACTUAL."
@@ -22,8 +24,36 @@
   [actual ideal]
   (mapv - actual ideal))
 
+(defn- cross-entropy
+  "The cross entropy cost function is defined as
+   - (sum [e_i * ln (a_i) + (1 - e_i) ln (1 - a_j).
+  where E = [e_0 ... e_j] is the expected value and
+  A = [a_0 ... a_j] is the observed value."
+  [actual ideal]
+  (let [xs (mapv #(* %1 (Math/log %2)) ideal actual)
+        ys (mapv #(* (- 1.0 %1) (Math/log (- 1.0 %2))) ideal actual)]
+    (- (reduce + (into ys xs)))))
+
+; TODO: at times the gradient of the cross entropy function results in a divison
+; by zero error. The ideal workaround would be to combine the softmax and
+; cross-entropy calculation to prevent extreme values in the calculation. For now
+; cross-entropy-grad will replace 0's in its denominator with small values
+(defn- cross-entropy-grad
+  "Given expected output E and observed output A
+  the gradient of the cross entropy function is V
+  where v_j = (a_j - e_j)/(1 - a_j)a_j."
+  [actual ideal]
+  (mapv #(/ %1 %2)
+        (mapv - actual ideal)
+        (mapv
+         (fn [a] (if (< a 0.01)
+                   0.01
+                   (* a (- 1.0 a))))
+           actual)))
+
 (def cost-fns
-  {:mean-squared [mean-squared mean-squared-grad]})
+  {:mean-squared [mean-squared mean-squared-grad]
+   :cross-entropy [cross-entropy cross-entropy-grad]})
 
 (defn get-cost-fn
   [kw]
