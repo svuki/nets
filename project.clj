423b1299d529984c2dae194874109611cf204fc0
Stefan Vukicevic <stefanvuk17@gmail.com>
2018-01-30 23:37:01 -0800

    Add project description and url.

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject nets "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Stochastic gradient descent for multi layer perceptrons"
+  :url "https://github.com/svuki/nets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/algo.generic "0.1.2"]
                  [net.mikera/core.matrix "0.61.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [clojurewerkz/buffy "1.1.0"]]
+                 [clatrix "0.5.0"]
+                 [uncomplicate/neanderthal "0.18.0"]]
   :main ^:skip-aot nets.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
