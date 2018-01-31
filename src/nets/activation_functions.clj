c1ef1ea14686bce2888592db00a6ff08269d2af1
Stefan Vukicevic <stefanvuk17@gmail.com>
2018-01-30 18:41:50 -0800

    Rewrite functions to conform with core.matrix API.

--- src/nets/activation_functions.clj
+++ src/nets/activation_functions.clj
@@ -5,22 +5,14 @@
 (ns nets.activation-functions
   (:require [clojure.algo.generic.math-functions :as math]
             [clojure.core.matrix :as matrix]
+            [nets.utilis :as utils]
             [nets.matrix-utils :as m-utils])
   (:gen-class))
 
 (defn- vectorize
   "Takes a function and returns its vectorized form."
   [func]
-  (fn [v] (mapv func v)))
-
-(defn no-NaN?
-  "Ensures x is not NaN"
-  [x]
-  (not (Double/isNaN x)))
-(defn no-NaNs?
-  "Ensures no element in v is NaN"
-  [v]
-  (not-any? #(Double/isNaN %) v))
+  (fn [v] (matrix/emap func v)))
 
 (defn sigmoid [x] (/ 1.0 (+ 1 (math/exp (- x)))))
 (defn sigmoid-deriv [x] (* (sigmoid x) (- 1 (sigmoid x))))
@@ -52,7 +44,7 @@
    function. Given argument v of dimension K, the i_th component of the
    return vector is (e^(v_i) / (sum (j = 0 to K) e^(v_j))."
   [v]
-  {:post [(no-NaNs? %)]}
+  {:post [(utils/no-NaNs? %)]}
   ; to avoid overflows we subtract the largest x from each of the values
   ; before exponentiating. Note that this does not change the value of
   ; the output
@@ -61,12 +53,9 @@
         sum (apply + exps)]
     (mapv #(/ % sum) exps)))
 
-;;; TODO: when associated with certain loss functions, the computation of
-;;; the soft-max "gradient" can be simplified. Detect when such
-;;; such functions are used in unison and do the cheaper computatoins
-
-
-;; TODO: check that this works
+;; TODO: sloppy. In what cases would we use softmax without cross entropy? Would we ever use
+;; it in a hidden layer? Right now cross-entropy/softmax on output is handled by a seperate
+;; routine so the softmax-jacobian is never calculated
 (defn- softmax-jacobian
   "Given a vector v, returns the tranpose of the jacobian matrix
   of the softmax function evaluated at v."
@@ -81,13 +70,13 @@
                                      (- (m-utils/kdelta i j)
                                 (nth v j)))))))
 
-(def ^:dynamic act-fns (transient {}))
-(assoc! act-fns :sigmoid (mapv vectorize [sigmoid sigmoid-deriv]))
-(assoc! act-fns :tanh (mapv vectorize [tanh tanh-deriv]))
-(assoc! act-fns :relu (mapv vectorize [relu relu-deriv]))
-(assoc! act-fns :leaky-relu (mapv vectorize [leaky-relu leaky-relu-deriv]))
-(assoc! act-fns :softmax [softmax softmax-jacobian])
-(assoc! act-fns :softplus (mapv vectorize [softplus softplus-grad]))
+(def act-fns
+  {:sigmoid (mapv vectorize [sigmoid sigmoid-deriv])
+   :tanh (mapv vectorize [tanh tanh-deriv])
+   :relu (mapv vectorize [relu relu-deriv])
+   :leaky-relu (mapv vectorize [leaky-relu leaky-relu-deriv])
+   :softmax [softmax softmax-jacobian]
+   :softplus (mapv vectorize [softplus softplus-grad])})
 
 (defn get-fn
   "Returns the function associated with NAME. Returns nil if no function is found."
@@ -99,8 +88,3 @@
   "Returns the derivativ eof the function associated with NAME. Returns nil if no function is found."
   [name]
   (second (get act-fns name)))
-
-(defn add-function
-  "Enables the function FN and its jacobian/gradient to be found using get-fn and get-deriv."
-  [name fn deriv]
-  (assoc! act-fns name [fn deriv]))
