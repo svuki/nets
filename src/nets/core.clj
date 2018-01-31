b77b51ba557a1c0311a5abc98bd5bd18b0b571ab
Stefan Vukicevic <stefanvuk17@gmail.com>
2018-01-30 23:34:55 -0800

    Change main to compute sin function.

--- src/nets/core.clj
+++ src/nets/core.clj
@@ -2,7 +2,8 @@
   (:require [nets.examples :as ex]))
 
 (defn -main
-  [ iterations ]
+  []
   (printf "In the future this will provide an interactive environment for constructing and training nets.
-           For Now all it does is compute the floor function. Enjoy\n")
-  (ex/floor-example 1000))
+For Now all it does is compute the sin function. I suggest running it for 10,000+ iterations. Enjoy\n")
+  (flush)
+  (ex/sin-example 1))
